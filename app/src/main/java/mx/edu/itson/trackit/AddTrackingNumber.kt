8bcf40111921d9be14f023dc90254169f80292e6
Luis Angel Marin <la.marin@outlook.com>
2023-03-27 22:56:22 -0700

    Add Tracking Number Added
    Added Add Tracking Number and navigation

---
+++ app/src/main/java/mx/edu/itson/trackit/AddTrackingNumber.kt
@@ -0,0 +1,28 @@
+package mx.edu.itson.trackit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class AddTrackingNumber : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_tracking_number)
+
+        var btnTrackPackage: Button = findViewById(R.id.btnAddTrackingNumber_trackPackage)
+
+        btnTrackPackage.setOnClickListener(){
+            var intent: Intent = Intent(this , MainPage::class.java)
+            startActivity(intent)
+        }
+
+        var ibReturnMainPage: ImageButton = findViewById(R.id.ibAddTrackingNumber_back)
+
+        ibReturnMainPage.setOnClickListener(){
+            var intent: Intent = Intent(this , MainPage::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/mx/edu/itson/trackit/MainPage.kt
+++ app/src/main/java/mx/edu/itson/trackit/MainPage.kt
@@ -1,11 +1,37 @@
 package mx.edu.itson.trackit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        var btnAddTrackingNumber: Button = findViewById(R.id.btnMainPage_addParcel)
+
+        btnAddTrackingNumber.setOnClickListener(){
+            var intent: Intent = Intent(this , AddTrackingNumber::class.java)
+            startActivity(intent)
+        }
+
+        var myTrackings: ImageButton = findViewById(R.id.ibMainPage_myTrackings)
+
+        myTrackings.setOnClickListener(){
+            var intent: Intent = Intent(this , AddTrackingNumber::class.java)
+            startActivity(intent)
+        }
+
+        var myAccount: ImageButton = findViewById(R.id.ibMainPage_myAccount)
+
+        myAccount.setOnClickListener(){
+            var intent: Intent = Intent(this , AddTrackingNumber::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }

4933e49aec4c5bb2dafe878714fa31ebf6b904d8
rjsaa <rjsaavedran@gmail.com>
2023-05-08 18:06:19 -0700

    cambia data class de envio , consume la api de dhl al momento de presionar aniadir numero de rastreo e imprime el resultado de ejemplo

--- app/src/main/java/mx/edu/itson/trackit/AddTrackingNumber.kt
+++ app/src/main/java/mx/edu/itson/trackit/AddTrackingNumber.kt
@@ -4,9 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import mx.edu.itson.trackit.data.Envio
+import okhttp3.*
+import java.io.IOException
 
 class AddTrackingNumber : AppCompatActivity() {
+
+    val client = OkHttpClient()
+    private val database = Firebase.firestore
+    private val enviosRef = database.collection("envios")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_tracking_number)
@@ -14,8 +25,14 @@ class AddTrackingNumber : AppCompatActivity() {
         var btnTrackPackage: Button = findViewById(R.id.btnAddTrackingNumber_trackPackage)
 
         btnTrackPackage.setOnClickListener(){
-            var intent: Intent = Intent(this , MainPage::class.java)
-            startActivity(intent)
+
+            this.run()
+
+            //tracking number de prueba
+            //this.addTracking("00340434292135100186")
+
+            //var intent: Intent = Intent(this , MainPage::class.java)
+            //startActivity(intent)
         }
 
         var ibReturnMainPage: ImageButton = findViewById(R.id.ibAddTrackingNumber_back)
@@ -31,5 +48,67 @@ class AddTrackingNumber : AppCompatActivity() {
             var intent: Intent = Intent(this , ScanQRCode::class.java)
             startActivity(intent)
         }
+    }
+
+    fun addTracking(number: String){
+       var trkNumber = number
+
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("https://api-test.dhl.com/track/shipments?trackingNumber= "+trkNumber)
+            .get()
+            .addHeader("DHL-API-Key", "uaJIX8pdM34mMAlFHRkMJqcXyuCK2lRg")
+            .build()
+
+
+        val response = client.newCall(request).execute()
+        println(response.body.toString())
+
+    }
+
+    fun run() {
+        val request = Request.Builder()
+            .url("https://api-eu.dhl.com/track/shipments?trackingNumber=12345678910")
+            .get()
+            .addHeader("DHL-API-Key","uaJIX8pdM34mMAlFHRkMJqcXyuCK2lRg")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    var trk: EditText = findViewById(R.id.etAddTrackingNumber_trackingNumber) as EditText
+
+                    val bodyString: String? = response.body?.string()
+
+                    println(bodyString)
+
+/*
+                    var TrackId = trk.text
+                    var Paqueteria = "DHL"
+                    var Destino =
+                    var PuntosControl =
+                    var Estado = bodyString
+                    var EsArchivado =
+
+                    val envio = Envio(
+
+                    )
+
+
+                    
+*/
+                }
+            }
+        })
+
+        var intent: Intent = Intent(this , tracking_menu::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/mx/edu/itson/trackit/data/Envio.kt
+++ app/src/main/java/mx/edu/itson/trackit/data/Envio.kt
@@ -1,5 +1,5 @@
 package mx.edu.itson.trackit.data
 
-data class Envio(var Id:Int, var TrackId: Int, var Paqueteria: String,
+data class Envio(var Id:String?, var TrackId: String, var Paqueteria: String,
                  var Destino:String, var PuntosControl: String,
                  var Estado: String, var EsArchivado: Boolean)
